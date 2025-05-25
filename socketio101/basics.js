313c1120ec115b42d5ff90a01f82ce3037068316
NSPOLE01 <89700736+NSPOLE01@users.noreply.github.com>
2025-05-24 20:14:54 -0400

    second commit

---
+++ socketio101/basics.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const socketio = require("socket.io");
+
+app.use(express.static(__dirname + "/public"));
+
+const expressServer = app.listen(8000);
+const io = socketio(expressServer);
+
+io.on("connection", (socket) => {
+  console.log(socket.id, "has connected");
+  socket.emit("messageFromServer", { data: "Welcome to the socket server!" });
+  socket.on("messageFromClient", (data) => {
+    console.log("Data:", data);
+  });
+});
